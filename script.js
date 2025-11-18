046b45c0484db7a6f7da4b004f3828b393ae805f
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2025-11-18 16:39:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -49,6 +49,27 @@ function populateUI() {
     }
 }
 
+// Movie select event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
+    updateSelectedCount();
+});
+
+// Seat click event
+container.addEventListener('click', e => {
+    if (
+        e.target.classList.contains('seat') &&
+        !e.target.classList.contains('occupied')
+    ) {
+        e.target.classList.toggle('selected');
+
+        updateSelectedCount();
+    }
+});
+
+// Initial count and total set
+updateSelectedCount();
 
 
 
